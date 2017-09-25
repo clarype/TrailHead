01ef5d593176a896622e56d1e7937422456c5f59
clarype <clarype@oregonstate.edu>
2017-09-25 11:34:04 -0700

    click lat long

--- HikeToJornLake/HikeToJornLake.js
+++ HikeToJornLake/HikeToJornLake.js
@@ -17,7 +17,16 @@
                     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 }).addTo(map);
 
+                var popup = L.popup();
 
+                function onMapClick(e) {
+                    popup
+                        .setLatLng(e.latlng)
+                        .setContent("You clicked the map at " + e.latlng.toString())
+                        .openOn(map);
+                }
+
+                map.on('click', onMapClick);
 
 
 
