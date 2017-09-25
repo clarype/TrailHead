f44daf499cf9f81d8a3516f63b8b6ad59d9713b4
clarype <clarype@oregonstate.edu>
2017-09-25 10:48:16 -0700

    Add click function
    the function should give the lat and log.

--- home.js
+++ home.js
@@ -15,6 +15,17 @@
                 L.tileLayer('http://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
                     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 }).addTo(map);
+                
+                var popup = L.popup();
+
+                function onMapClick(e) {
+                    popup
+                        .setLatLng(e.latlng)
+                        .setContent("You clicked the map at " + e.latlng.toString())
+                        .openOn(map);
+                }
+
+                map.on('click', onMapClick);
 
 
 
