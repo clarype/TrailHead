b754232fffb893ba98d00b2e17fcdca18f140fe9
clarype <clarype@oregonstate.edu>
2017-10-29 15:16:02 -0700

    updated to geojson file type

--- HikeToEagleCap/HikeToEagleCap.js
+++ HikeToEagleCap/HikeToEagleCap.js
@@ -8,7 +8,7 @@
             breakpointPos: '25%',
             createMap: function () {
 
-                var map = L.map('map').setView([44.4457, -121.8484], 14);
+                var map = L.map('map').setView([45.2150, -117.3863], 13);
                 L.tileLayer('http://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
                     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 }).addTo(map);
