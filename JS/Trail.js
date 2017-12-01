7580bae675e636aad1e0805f281e5b9cea422508
clarype <clarype@oregonstate.edu>
2017-12-01 10:10:50 -0800

    50 to 25 scroll

--- JS/Trail.js
+++ JS/Trail.js
@@ -364,9 +364,9 @@
                 if (viewing.data("scene") !== $("section:last").data("scene")) {
 
                     if (nav.length == 0) {
-                        window.scrollBy(0, viewing.offset().top + viewing.height() - $(window).scrollTop() - $('.storymap-navbar').height() - 50);
+                        window.scrollBy(0, viewing.offset().top + viewing.height() - $(window).scrollTop() - $('.storymap-navbar').height() - 25);
                     } else {
-                        window.scrollBy(0, viewing.offset().top + viewing.height() - $(window).scrollTop() - 50);
+                        window.scrollBy(0, viewing.offset().top + viewing.height() - $(window).scrollTop() - 25);
                     }
                 } else {
                     window.scrollTo(0, 0);
